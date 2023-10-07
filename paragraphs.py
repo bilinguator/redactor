bbe2dd9ce686a545e16e7b5c83ee90c09b41840c
Dmitrii Snitkin <snitkin.d@list.ru>
2023-10-07 18:19:20 +0300

    Add docstrings to functions in paragraphs.py

--- paragraphs.py
+++ paragraphs.py
@@ -1,6 +1,14 @@
 import re
 
 def print_paragraphs (text, paragraphs, with_number=True, delimiter='\n\n'):
+    """Print specified paragraphs of the text.
+
+    str `text` - text, paragraphs of which need to be printed;
+    list/set/tuple `paragraphs` - set of numbers of paragraphs to print;
+    bool `with_number` - determines if numbers of paragraphs are printed;
+    str `delimiter` - string to be printed between paragraphs.
+    """
+
     text = text.split('\n')
     result = []
     for i in paragraphs:
@@ -9,6 +17,15 @@ def print_paragraphs (text, paragraphs, with_number=True, delimiter='\n\n'):
     print(delimiter.join(result))
     
 def search_in_paragraphs (text, query, case_matters=False):
+    """Get numbers of paragraphs containing a particular query substring with counts
+        of the query.
+    
+    str `text` - text where to search paragraphs and query;
+    str `query` - substring to be found in paragraphs;
+    bool `case_matters` - determines if case of th query matters;
+    result dict - key: paragraph number, value: count of `query` in the paragraph.
+    """
+
     text = text.split('\n')
     result = dict()
     for i in range(len(text)):
@@ -23,6 +40,13 @@ def search_in_paragraphs (text, query, case_matters=False):
     return result
 
 def print_paragraphs_by_query (text, query, case_matters=False):
+    """Print paragraphs containing a particular query substring.
+    
+    str `text` - text where to search paragraphs and query;
+    str `query` - substring to be found in paragraphs;
+    bool `case_matters` - determines if case of th query matters.
+    """
+    
     text = text.split('\n')
     result = []
     for i in range(len(text)):
@@ -41,6 +65,35 @@ def print_paragraphs_by_query (text, query, case_matters=False):
 
 def detect_chapters (text, chapter='', numbering='arabic', delimiter='', with_title=False,
                      numbering_first=False):
+    """Get and print paragraphs containing chapters' headings. 
+    
+    str `chapter` - string containing key word for a chapter, e.g.
+        "Chapter" for English, "Глава" for Russian, "Chapitre" for French,
+        etc.; word case matters here; the space character delimiting the key
+        word from a numeral is the part of this argument;
+    str `numbering` - string of one of the following variants:
+        - "arabic" for Arabic numbers ("1", "2", "3", etc.);
+        - "roman" for the Roman numbers ("I", "II", "III", "IV", etc.);
+        - "ja" or "zh" for Japanese or Chinese numerals ("一"、"二"、"三"
+            "四", etc.);
+        - "text" for non-numeric characters; suitable if numerals in words are
+            presented ("One", "Two", "Three", etc.)
+    str `delimiter` - delimiter among the chapter-numbering and title parts;
+    bool `with_title` - specifies if chapters have titles;
+    bool `numbering_first` - specifies if numbering precedes chapter key word
+        (e.g., "XIX chapter");
+    return set - set of numbers of paragraphs containing chapters' headings.
+
+    If the chapters' headings of your text look like "Chapter MMMCMXCIX — Epilogue",
+        specify arguments as follows:
+
+        chapter = 'Chapter ',
+        numbering = 'roman',
+        delimiter = ' — ',
+        with_title = True,
+        numbering_first = False.
+    """
+
     regex = '' if numbering_first else chapter
     if numbering == 'arabic':
         regex = f'{regex}[0-9]+'.strip()
@@ -71,16 +124,29 @@ def detect_chapters (text, chapter='', numbering='arabic', delimiter='', with_ti
         return set()
     
 def tag_paragraphs (text, paragraphs, tag='h1'):
-    text = text.split('\n')
+    """Enclose specified paragraphs of the text in tags.
+
+    str `text` - text in which to tag paragraphs;
+    list/set/tuple `paragraphs` - set of numbers of paragraphs to tag;
+    str `tag` - tag in which to enclose specified paragraphs;
+    return str - text with tagged paragraphs.
+    """
     
+    text = text.split('\n')
     for i in range(len(text)):
         if i in paragraphs:
             text[i] = text[i].strip()
             text[i] = f'<{tag}>{text[i]}</{tag}>'
-
     return '\n'.join(text)
 
 def search_paragraphs_by_regex (text, regex):
+    """Get numbers of paragraphs matching regular expression.
+
+    str `text` - text in which to search matching paragraphs;
+    str `regex` - regular expression used to search matching paragraphs;
+    return set - numbers of paragraphs matching regular expression.
+    """
+
     text = text.split('\n')
     paragraphs = []
     for i in range(len(text)):
@@ -89,6 +155,12 @@ def search_paragraphs_by_regex (text, regex):
     return set(paragraphs)
 
 def print_paragraphs_by_regex (text, regex):
+    """Print paragraphs matching regular expression.
+
+    str `text` - text in which to search matching paragraphs;
+    str `regex` - regular expression used to search matching paragraphs;
+    """
+
     text = text.split('\n')
     paragraphs = []
     for i in range(len(text)):
@@ -97,6 +169,18 @@ def print_paragraphs_by_regex (text, regex):
     print_paragraphs('\n'.join(text), paragraphs)
 
 def tag_characters (text, paragraphs, characters, dialogue_delimiter, tag='b'):
+    """Enclose characters names in the play to the specified tags.
+
+    str `text` - text in which to tag characters' names;
+    list/set/tuple `paragraphs` - set of numbers of paragraphs to in which to tag
+        characters' names;
+    list/set/tuple `characters` - set of characters' names;
+    str `dialogue_delimiter` - specifies what punctuation mark separates characters'
+        names from their speeches;
+    str `tag` - tag in which to enclose characters' names;
+    return str - text with characters' names tagged.
+    """
+    
     text = text.split('\n')
     characters = [c + dialogue_delimiter for c in characters]
     for i in paragraphs:
@@ -107,15 +191,26 @@ def tag_characters (text, paragraphs, characters, dialogue_delimiter, tag='b'):
     return '\n'.join(text)
 
 def remove_paragraphs (text, paragraphs):
+    """Remove paragraphs from the text.
+
+    str `text` - text in which to remove paragraphs;
+    list/set/tuple `paragraphs` - set of numbers of paragraphs to remove;
+    return str - text with paragraphs removed.
+    """
+
     text = text.split('\n')
-    
     for i in range(len(text)-1, -1, -1):
         if i in paragraphs:
             del text[i]
-            
     return '\n'.join(text)
 
 def count_repeated_paragraphs (text):
+    """Count repeated paragraphs.
+
+    str `text` - text in which to coint repeated paragraphs;
+    return dict - key: str with repeated paragraph, value - its count (int).
+    """
+
     text = text.split('\n')
     unique_paragraphs = set(text)
     repeated_paragraphs = dict()
@@ -128,6 +223,23 @@ def count_repeated_paragraphs (text):
     return repeated_paragraphs
 
 def replace_interactively (text, old, new, scope=55):
+    """Replace one substring to another in the text in th interaction mode.
+
+    str `text` - text in which to conduct replacement;
+    str `old` - substring to be replaced;
+    str `new` - replacement;
+    int `scope` - how many characters to print to the right and to the left
+        from the old substring;
+    return str - text with the result of replacement.
+
+    Usage:
+    text = replace_interactively(text, '“', '‘')
+    
+    Each hit of the old substring will be accompanied with the input field.
+    Leave the field empty if the replacement is not needed. Otherwise, type
+    anything. Press Enter to confirm your choise.
+    """
+
     old = old if type(old) == list else [old]
     for c in old:
         hits = re.finditer(re.escape(c), text)
